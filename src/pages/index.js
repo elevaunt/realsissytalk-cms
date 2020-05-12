5eaa473ecb4e9cba0bcfd25edf039489844c972a
Lee Porter <lee.porter@sprinklr.com>
2020-05-12 11:51:10 -0500

    fix: homepage styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styled from "@emotion/styled"
 import { HeroHuge } from "../components/compositions/Heroes"
 import { rhythm } from "../utils/typography"
 import PlainPageWrapper from "../components/compositions/PlainPageWrapper"
+import mq from "../utils/mediaQueries"
 
 const HomeSmall = styled('small')({
   display: "block",
@@ -21,29 +22,54 @@ const HomeTitle = styled(({ className }) => <span className={className}><HomeSma
   fontFamily: "cuisine,sans-serif",
   fontWeight: 500,
   fontStyle: "normal",
-  fontSize: rhythm(5)
+  fontSize: rhythm(4),
+  [mq.min.md]: {
+    fontSize: rhythm(5)
+  }
 })
-const HomeDivider = styled(({ className }) => {
-  // const [isMobile, setIsMobile] = useState(window.outerWidth < 768)
-  // let checkViewportWidth = () => {
-  //   if (window.outerWidth < 768) {
-  //     setIsMobile(true)
-  //   } else {
-  //     setIsMobile(false)
-  //   }
-  // }
-  // useEffect(() => {
-  //   window.addEventListener("resize", checkViewportWidth)
-  // }, [])
-  // console.log(isMobile)
-  return <span className={className}>|</span>
-})({
+const NoBreak = styled('span')({
+  whiteSpace: "nowrap"
+})
+const HomeDivider = styled(({ className }) => <span className={className}>&mdash;</span>)({
   display: "inline-block",
-  margin: `4px ${rhythm(1)} 0`,
+  margin: `4px ${rhythm(.5)} 0`,
   verticalAlign: "top",
-})
+  [mq.max.md]: {
+    display: "block",
+    color: "transparent",
+    height: rhythm(.5)
+  }
+}, ({hideMd, hideLg}) => ([
+  hideMd && {
+    [mq.max.md]: {
+      display: 'none'
+    }
+  },
+  hideLg && {
+    [`@media (min-width: 1110px)`]: {
+      display: 'none'
+    }
+  }
+]))
 const HomeSubtitle = styled(({ className }) => {
-  return <span className={className}>self <HomeEm>LOVE</HomeEm> <HomeDivider /> self <HomeEm>CARE</HomeEm> <HomeDivider /> self <HomeEm>EXPRESSION</HomeEm></span>
+  return (
+    <span className={className}>
+      <HomeDivider hideMd /> 
+      <NoBreak>self <HomeEm>LOVE</HomeEm></NoBreak> 
+      <HomeDivider /> 
+      <NoBreak>
+        self <HomeEm>CARE</HomeEm>
+      </NoBreak> 
+      <HomeDivider /> 
+      <NoBreak>
+      	<HomeDivider hideMd hideLg /> 
+      	<NoBreak>
+      	  self <HomeEm>EXPRESSION</HomeEm>
+      	</NoBreak>
+      	<HomeDivider hideMd /> 
+      </NoBreak>
+    </span>
+  )
 })({
   position: "relative",
   verticalAlign: "middle",
